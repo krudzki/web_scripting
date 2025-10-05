036d7783140ee5a60816ca739481b4a960ce0adb
Krzysztof Rudzki <krzysztof.r.rudzki@gmail.com>
2025-10-05 22:19:48 +0200

    save

--- DuckDuckGo e-mail/create_email_adress.py
+++ DuckDuckGo e-mail/create_email_adress.py
@@ -6,3 +6,5 @@ def generate_random_alias(length=10):
     """Generate a random alias of given length."""
     characters = string.ascii_letters + string.digits
     return ''.join(random.choice(characters) for _ in range(length))
+
+def generate_duck_aliases(count, filename="duck_aliases.txt"):
