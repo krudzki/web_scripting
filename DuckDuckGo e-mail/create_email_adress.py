ba0c9ba0089d74ab27f22a823611d2ba32f56f05
Krzysztof Rudzki <krzysztof.r.rudzki@gmail.com>
2025-10-07 22:12:11 +0200

    update

--- DuckDuckGo e-mail/create_email_adress.py
+++ DuckDuckGo e-mail/create_email_adress.py
@@ -8,3 +8,6 @@ def generate_random_alias(length=10):
     return ''.join(random.choice(characters) for _ in range(length))
 
 def generate_duck_aliases(count, filename="duck_aliases.txt"):
+    """Generate a specified number of DuckDuckGo email aliases and save them to a file."""
+    
+    # 8 characters is the default length of a randomly generated one-time DuckDuckGo address
